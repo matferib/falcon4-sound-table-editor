492c17b4fb0b44075693ed12113859b6ec2804f0
Gustavo <gsmeirelles@gmail.com>
2022-12-22 18:40:34 -0300

    add Choose UI menu

--- main.cpp
+++ main.cpp
@@ -3,67 +3,76 @@
 #include <memory>
 #include <string>
 
-
 namespace {
+  // The UI abstraction is contained here. Subclasses implement the look and feel.
+  class GenericUI {
+  public:
+    virtual ~GenericUI() {}
 
-// The UI abstraction is contained here. Subclasses implement the look and feel.
-class GenericUI {
-public:
-  virtual ~GenericUI() {}
+    // Fires up any initialization of the UI and runs until the program ends.
+    virtual void Run() = 0;
+  };
 
-  // Fires up any initialization of the UI and runs until the program ends.
-  virtual void Run() = 0;
-};
+  // TODO(seifer): move this to a separate module to avoid polluting the main one.
+  // An example class implementing GenericUI in command line style (CommandLineUI).
+  class CommandLineUI : public GenericUI {
+  public:
+    ~CommandLineUI() override {}
 
-// TODO(seifer): move this to a separate module to avoid polluting the main one.
-// An example class implementing GenericUI in command line style (CommandLineUI).
-class CommandLineUI : public GenericUI {
-public:
-  ~CommandLineUI() override {}
+    void Run() override {
+      enum Options {
+        OPTION_QUIT = 1
+      };
+      do {
+        std::cout << "Please, select an option:" << std::endl;
+        std::cout << "1: quit." << std::endl;
+        int option_selected;
+        std::cin >> option_selected;
+        if (std::cin.eof()) {
+          break;
+        }
+        if (!std::cin) {
+          std::cout << "Bad input..." << std::endl;
+          std::cin.clear();
+          std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+          continue;
+        }
+        if (option_selected == OPTION_QUIT) {
+          break;
+        }
+        std::cout << "You chose option: " << option_selected << ", unknown" << std::endl;
+      } while (true);
+    }
+  };
 
-  void Run() override {
-    enum Options {
-      OPTION_QUIT = 1
+  // TODO(sassah): implement a windows UI (WindowsUI) that inherits from GenericUI.
+  class WindowsUI : public GenericUI {
+  public:
+    ~WindowsUI() override {}
+
+    void Run() override {}
+  };
+
+  // This is the main UI entry (factory). It is the one which picks which UI it should start.
+  std::unique_ptr<GenericUI> CreateUI(int argc, char** argv) {
+    enum Option_UI {
+      WINDOWS_UI = 1,
+      COMMANDLINE_UI = 2
     };
-    do {
-      std::cout << "Please, select an option:" << std::endl;
-      std::cout << "1: quit." << std::endl;
-      int option_selected;
-      std::cin >> option_selected;
-      if (std::cin.eof()) {
-        break;
-      }
-      if (!std::cin) {
-        std::cout << "Bad input..." << std::endl;
-        std::cin.clear();
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        continue;
-      }
-      if (option_selected == OPTION_QUIT) {
-        break;
-      }
-      std::cout << "You chose option: " << option_selected << ", unknown" << std::endl;
-    } while (true);
+    std::cout << "Type 1 for Windows or 2 for Command Line:" << std::endl;
+    int ChooseUI{};
+    std::cin >> ChooseUI;
+    if (ChooseUI == WINDOWS_UI) {
+      return std::make_unique<WindowsUI>();
+    }
+    else if (ChooseUI == COMMANDLINE_UI) {
+      return std::make_unique<CommandLineUI>();
+    }
+    else {
+      std::cout << "You chose option: " << ChooseUI << ", is unknown!" << std::endl;
+    }
   }
-};
-
-// TODO(sassah): implement a windows UI (WindowUI) that inherits from GenericUI.
-class WindowUI : public GenericUI {
-public:
-  ~WindowUI() override {}
-
-  void Run() override {}
-};
-
-
-// This is the main UI entry (factory). It is the one which picks which UI it should start.
-std::unique_ptr<GenericUI> CreateUI(int argc, char** argv) {
-  // TODO(sassah): add you UI constructor here.
-  return std::make_unique<CommandLineUI>();
-}
-
 }  // namespace
-
 
 int main(int argc, char** argv) {
   auto ifg = CreateUI(argc, argv);
